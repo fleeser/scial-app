16c26d971c2daef3290d256f34540af92d70d638
Florian Leeser <florian.leeser@icloud.com>
2021-08-11 00:08:50 +0200

    :electric_plug: Added Firebase initialization
    Added code for initialization of Firebase on all supported platforms.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firedart/firedart.dart' as FirebaseAuthWindowsLinux;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(ProviderScope(child: App()));
+import 'package:scial/exclusives/preferences_store.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || !Platform.isLinux) FirebaseAuthWindowsLinux.FirebaseAuth('AIzaSyDm9nOXkJAHXrwe3Tm9TIX4GAlSQjGC_og', await PreferencesStore.create());
+  else await Firebase.initializeApp();
+
+  runApp(ProviderScope(child: App()));
+}
 
 class App extends StatelessWidget {
 
@@ -23,6 +36,6 @@ class Root extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: Colors.yellow);
   }
 }
