ba4508b86c5f8775be4d698ed94ae7ef96ba7951
Florian Leeser <florian.leeser@icloud.com>
2021-08-11 00:50:48 +0200

    :electric_plug: Wrapped App in EasyLocalization
    Wrapped the complete widget tree inside of an EasyLocalization widget to enable localization based on the system settings.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart' as FirebaseCoreStandard;
 import 'package:firedart/firedart.dart' as FirebaseWindowsLinux;
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:scial/exclusives/preferences_store.dart';
@@ -14,7 +15,21 @@ void main() async {
   if (Platform.isWindows || !Platform.isLinux) FirebaseWindowsLinux.FirebaseAuth('AIzaSyDm9nOXkJAHXrwe3Tm9TIX4GAlSQjGC_og', await PreferencesStore.create());
   else await FirebaseCoreStandard.Firebase.initializeApp();
 
-  runApp(ProviderScope(child: App()));
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('de', 'DE')
+        ],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en', 'US'),
+        child: App()
+      )
+    )
+  );
 }
 
 class App extends StatelessWidget {
@@ -24,6 +39,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       home: Root()
     );
