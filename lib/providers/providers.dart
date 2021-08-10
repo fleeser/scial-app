e00677d1e5e6ebf30e225f0de515a44848e2b2f9
Florian Leeser <florian.leeser@icloud.com>
2021-08-11 01:37:57 +0200

    :necktie: Created AuthService provider
    Created a provider responsible for holding an instance of the AuthService.

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,7 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuthStandard;
 import 'package:firedart/firedart.dart' as FirebaseWindowsLinux;
 
+import 'package:scial/services/auth_service.dart';
+
 // FIREBASE
 
 final firebaseStandardAuthProvider = Provider<FirebaseAuthStandard.FirebaseAuth>((ref) => FirebaseAuthStandard.FirebaseAuth.instance);
 final firebaseWindowsLinuxAuthProvider = Provider<FirebaseWindowsLinux.FirebaseAuth>((ref) => FirebaseWindowsLinux.FirebaseAuth.instance);
+
+// SERVICES
+
+final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref.read));
