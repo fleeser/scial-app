09f1824309918a6240895989134c2a9526fc190a
Florian Leeser <florian.leeser@icloud.com>
2021-08-11 12:29:59 +0200

    :iphone: Added minimum window size
    Added a minimum window size to the desktop applications.

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,16 @@ import 'package:firedart/firedart.dart' as FirebaseWindowsLinux;
 import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuthStandard;
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 import 'package:scial/exclusives/preferences_store.dart';
 import 'package:scial/providers/providers.dart';
+import 'package:scial/screens/auth/sign_in/sign_in_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (Platform.isWindows || !Platform.isLinux) FirebaseWindowsLinux.FirebaseAuth.initialize('AIzaSyDm9nOXkJAHXrwe3Tm9TIX4GAlSQjGC_og', await PreferencesStore.create());
+  if (Platform.isWindows || Platform.isLinux) FirebaseWindowsLinux.FirebaseAuth.initialize('AIzaSyDm9nOXkJAHXrwe3Tm9TIX4GAlSQjGC_og', await PreferencesStore.create());
   else await FirebaseCoreStandard.Firebase.initializeApp();
 
   await EasyLocalization.ensureInitialized();
@@ -32,6 +34,15 @@ void main() async {
       )
     )
   );
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    doWhenWindowReady(() {
+      appWindow.minSize = Size(600, 450);
+      appWindow.size = Size(600, 450);
+      appWindow.alignment = Alignment.center;
+      appWindow.show();
+    });
+  }
 }
 
 class App extends StatelessWidget {
@@ -62,7 +73,7 @@ class Root extends ConsumerWidget {
 
       return authStateWindowsLinux.when(
         data: (bool isSignedIn) => isSignedIn ? Container(color: Colors.green) : Container(color: Colors.blue), 
-        loading: () => Container(color: Colors.yellow), 
+        loading: () => SignInScreen(), 
         error: (Object e, StackTrace? s) => Container(color: Colors.red)
       );
     }
@@ -70,9 +81,9 @@ class Root extends ConsumerWidget {
     final AsyncValue<FirebaseAuthStandard.User?> authStateStandard = watch(authStateStandardProvider);
 
     return authStateStandard.when(
-      data: (FirebaseAuthStandard.User? user) => user != null ? Container(color: Colors.green) : Container(color: Colors.blue),
+      data: (FirebaseAuthStandard.User? user) => user != null ? Container(color: Colors.green) : SignInScreen(),
       loading: () => Container(color: Colors.yellow), 
-      error: (Object e, StackTrace? s) => Container(color: Colors.red)
+      error: (Object e, StackTrace? s) => Container(color: Colors.red, child: Center(child: Text(e.toString()),),)
     );
   }
 }
