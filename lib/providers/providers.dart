e0190e745970503c5c13b364b389676a3b4e9bd1
Florian Leeser <florian.leeser@icloud.com>
2021-08-11 19:04:07 +0200

    :necktie: Created SignInIsLoadingProvider
    Created a provider that keeps track of wether the application currently processes the sign in logic or not.

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuthStandard;
 import 'package:firedart/firedart.dart' as FirebaseWindowsLinux;
 
 import 'package:scial/services/auth_service.dart';
+import 'package:scial/providers/provider_classes.dart';
 
 // FIREBASE
 
@@ -17,3 +18,7 @@ final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref.read)
 
 final authStateStandardProvider = StreamProvider<FirebaseAuthStandard.User?>((ref) => ref.watch(authServiceProvider).authStateChangesStandard);
 final authStateWindowsLinuxProvider = StreamProvider<bool>((ref) => ref.watch(authServiceProvider).signInStateWindowsLinux);
+
+// SIGN IN
+
+final signInIsLoadingProvider = StateNotifierProvider.autoDispose<BooleanStartingWithFalseStateNotifier, bool>((ref) => BooleanStartingWithFalseStateNotifier());
