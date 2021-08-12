9ad234dad6ec97813cdc729d52750296d7b37ebf
Florian Leeser <florian.leeser@icloud.com>
2021-08-12 16:46:42 +0200

    :100: Introducing user location
    The Map now moves to the user's current location on start up.

--- lib/screens/home/custom_map.dart
+++ lib/screens/home/custom_map.dart
@@ -2,28 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
+import 'package:scial/providers/providers.dart';
 import 'package:scial/themes/custom_system_ui_overlay_styles.dart';
 import 'package:scial/themes/palette.dart';
 
-class CustomMap extends StatelessWidget {
+class CustomMap extends ConsumerWidget {
+
+  const CustomMap({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+
+    final AsyncValue<LocationData> locationData = watch(currentLocationFutureProvider);
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: CustomSystemUiOverlayStyles.dark,
       child: Scaffold(
         backgroundColor: Palette.gray900,
         body: FlutterMap(
-          options: MapOptions(center: LatLng(66.5, -0.09)),
+          options: MapOptions(
+            center: LatLng(51.0, -0.09),
+            onMapCreated: (MapController controller) async {
+              LocationData locationData = await context.read(currentLocationFutureProvider.future);
+
+              if (locationData.latitude != null && locationData.longitude != null) {
+                controller.move(LatLng(locationData.latitude!, locationData.longitude!), 13.0);
+              }
+            }
+          ),
           layers: [
             TileLayerOptions(
-              urlTemplate: 'https://api.mapbox.com/styles/v1/fleeser/cks82ifob6r6018qqipcccm1s/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
+              urlTemplate: 'https://api.mapbox.com/styles/v1/fleeser/cks82ifob6r6018qqipcccm1s/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M4dzV3OTAwa280Mm5wZnI3Nndjc2cxIn0.hkto7RqB_ZxhzLR7hy5law',
               additionalOptions: {
-                'accessToken' : 'pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
+                'accessToken' : 'pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M4dzV3OTAwa280Mm5wZnI3Nndjc2cxIn0.hkto7RqB_ZxhzLR7hy5law',
                 'id' : 'mapbox.mapbox-streets-v8'
               }
+            ),
+            MarkerLayerOptions(
+              markers: locationData.when(
+                data: (LocationData locationData) {
+                  if (locationData.latitude != null && locationData.longitude != null) {
+                    return <Marker>[
+                      Marker(
+                        point: LatLng(locationData.latitude!, locationData.longitude!),
+                        width: 14.0,
+                        height: 14.0,
+                        builder: (BuildContext context) => Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Palette.blue500)) 
+                      )
+                    ];
+                  }
+
+                  return <Marker>[];
+                }, 
+                loading: () => <Marker>[], 
+                error: (Object e, StackTrace? s) => <Marker>[]
+              )
             )
           ]
         )
