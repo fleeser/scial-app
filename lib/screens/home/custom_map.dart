b3f3661776dc69e736ef85aa96c47a429d2f3ad0
Florian Leeser <florian.leeser@icloud.com>
2021-08-12 13:09:34 +0200

    :lipstick: Added SystemUiOverlayStyle
    Added a custom SystemUiOverlayStyle to the Map.

--- lib/screens/home/custom_map.dart
+++ lib/screens/home/custom_map.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class CustomMap extends StatelessWidget {
+import 'package:scial/themes/custom_system_ui_overlay_styles.dart';
+import 'package:scial/themes/palette.dart';
 
-  const CustomMap({ Key? key }) : super(key: key);
+class CustomMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: 'https://api.mapbox.com/styles/v1/fleeser/cks82ifob6r6018qqipcccm1s/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
-          additionalOptions: {
-            'accessToken' : 'pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
-            'id' : 'mapbox.mapbox-streets-v8'
-          }
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: CustomSystemUiOverlayStyles.dark,
+      child: Scaffold(
+        backgroundColor: Palette.gray900,
+        body: FlutterMap(
+          options: MapOptions(center: LatLng(66.5, -0.09)),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: 'https://api.mapbox.com/styles/v1/fleeser/cks82ifob6r6018qqipcccm1s/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
+              additionalOptions: {
+                'accessToken' : 'pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M2dG9jeWYxNm5mMnBwaHphMWk1OXUxIn0.YwaINf8LeWwLzeUtmsRPSQ',
+                'id' : 'mapbox.mapbox-streets-v8'
+              }
+            )
+          ]
         )
-      ]
+      )
     );
   }
 }
