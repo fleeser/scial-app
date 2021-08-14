be1a7065a7f56f29d1f30c2bab948d3f451d0bac
Florian Leeser <florian.leeser@icloud.com>
2021-08-14 13:50:33 +0200

    :necktie: Created EventModel
    Created a model for mapping the retrieved data from firestore.

---
+++ lib/models/event_model.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:scial/enums/event_category_enum.dart';
+import 'package:scial/extensions/event_category_extension.dart';
+
+class EventModel {
+  final double latitude;
+  final double longitude;
+  final String title;
+  final EventCategoryEnum eventCategoryEnum;
+
+  const EventModel({
+    required this.latitude,
+    required this.longitude,
+    required this.title,
+    required this.eventCategoryEnum
+  });
+
+  factory EventModel.fromMap(Map<String, dynamic> map) {
+    return EventModel(
+      latitude: (Map<String, dynamic>.from(map['position'])['geopoint'] as GeoPoint).latitude,
+      longitude: (Map<String, dynamic>.from(map['position'])['geopoint'] as GeoPoint).longitude,
+      title: map['title'],
+      eventCategoryEnum: categoryFromValue(map['category'])
+    );
+  }
+}
