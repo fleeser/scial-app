c801908fcf2ed9669fb7b034312d49bb0b0883f3
Florian Leeser <florian.leeser@icloud.com>
2021-08-14 13:45:16 +0200

    :necktie: Created eventsWithinStreamProvider
    Created a provider that is responsible for streaming events within a given radius from another given center.

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,14 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:location/location.dart';
 
+import 'package:scial/models/event_model.dart';
 import 'package:scial/services/auth_service.dart';
 import 'package:scial/providers/provider_classes.dart';
+import 'package:scial/services/database_service.dart';
 import 'package:scial/services/location_service.dart';
 
 // FIREBASE
 
 final firebaseAuthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+
+// GEO
+
+final geoflutterfireProvider = Provider<Geoflutterfire>((ref) => Geoflutterfire());
 
 // LOCATION
 
@@ -17,6 +26,8 @@ final locationProvider = Provider<Location>((ref) => Location.instance);
 // SERVICES
 
 final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref.read));
+
+final databaseServiceProvider = Provider<DatabaseService>((ref) => DatabaseService(ref.read));
 
 final locationServiceProvider = Provider<LocationService>((ref) => LocationService(ref.read));
 
@@ -33,3 +44,13 @@ final signInObscureTextProvider = StateNotifierProvider.autoDispose<BooleanStart
 // HOME
 
 final currentLocationFutureProvider = FutureProvider<LocationData>((ref) => ref.watch(locationServiceProvider).currentLocation);
+
+final eventsWithinStreamProvider = StreamProvider<List<EventModel>>((ref) {
+  final AsyncValue<LocationData> locationData = ref.watch(currentLocationFutureProvider);
+
+  if (locationData.data != null && locationData.data!.value.latitude != null && locationData.data!.value.longitude != null) {
+    return ref.read(databaseServiceProvider).streamEventsWithin(center: GeoFirePoint(locationData.data!.value.latitude!, locationData.data!.value.longitude!), radius: double.infinity);
+  }
+
+  return Stream.value(<EventModel>[]);
+});
