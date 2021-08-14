067f7669e74f8372fbf97c593d193b4f35e25ea9
Florian Leeser <florian.leeser@icloud.com>
2021-08-14 14:13:43 +0200

    :necktie: Added pins
    Added the logic for adding pins to the map that are within the given radius.

--- lib/widgets/home/map/custom_map.dart
+++ lib/widgets/home/map/custom_map.dart
@@ -6,9 +6,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 
+import 'package:scial/models/event_model.dart';
 import 'package:scial/providers/providers.dart';
 import 'package:scial/themes/custom_system_ui_overlay_styles.dart';
 import 'package:scial/themes/palette.dart';
+import 'package:scial/extensions/event_category_extension.dart';
 
 class CustomMap extends ConsumerWidget {
 
@@ -18,6 +20,7 @@ class CustomMap extends ConsumerWidget {
   Widget build(BuildContext context, ScopedReader watch) {
 
     final AsyncValue<LocationData> locationData = watch(currentLocationFutureProvider);
+    final AsyncValue<List<EventModel>> events = watch(eventsWithinStreamProvider);
 
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: CustomSystemUiOverlayStyles.dark,
@@ -46,14 +49,25 @@ class CustomMap extends ConsumerWidget {
               markers: locationData.when(
                 data: (LocationData locationData) {
                   if (locationData.latitude != null && locationData.longitude != null) {
-                    return <Marker>[
-                      Marker(
-                        point: LatLng(locationData.latitude!, locationData.longitude!),
-                        width: 14.0,
-                        height: 14.0,
-                        builder: (BuildContext context) => Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Palette.blue500)) 
-                      )
-                    ];
+                    return events.when(
+                      data: (List<EventModel> eventsList) => markersFor(latitude: locationData.latitude!, longitude: locationData.longitude!, eventsList: eventsList), 
+                      loading: () => <Marker>[
+                        Marker(
+                          point: LatLng(locationData.latitude!, locationData.longitude!),
+                          width: 14.0,
+                          height: 14.0,
+                          builder: (BuildContext context) => Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Palette.blue500)) 
+                        )
+                      ], 
+                      error: (Object e, StackTrace? s) => <Marker>[
+                        Marker(
+                          point: LatLng(locationData.latitude!, locationData.longitude!),
+                          width: 14.0,
+                          height: 14.0,
+                          builder: (BuildContext context) => Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Palette.blue500)) 
+                        )
+                      ]
+                    );
                   }
 
                   return <Marker>[];
@@ -67,4 +81,29 @@ class CustomMap extends ConsumerWidget {
       )
     );
   }
+}
+
+List<Marker> markersFor({ required double latitude, required double longitude, required List<EventModel> eventsList }) {
+  List<Marker> list = <Marker>[
+    Marker(
+      point: LatLng(latitude, longitude),
+      width: 14.0,
+      height: 14.0,
+      builder: (BuildContext context) => Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Palette.blue500)) 
+    )            
+  ];
+
+  eventsList.forEach((EventModel event) {
+    list.add(Marker(
+      point: LatLng(event.latitude, event.longitude),
+      width: 36.0,
+      height: 36.0,
+      builder: (BuildContext context) => Image(
+        image: AssetImage('assets/icons/pin.png'),
+        color: event.eventCategoryEnum.color
+      )
+    ));
+  });
+
+  return list;
 }
