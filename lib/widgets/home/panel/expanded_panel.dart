433f2cb21a2b6b4195c1e428192553a2076ea26d
Florian Leeser <florian.leeser@icloud.com>
2021-08-15 15:25:37 +0200

    :recycle: Refactored EventList
    Refactored the way the EventList is displayed on the expanded panel.

--- lib/widgets/home/panel/expanded_panel.dart
+++ lib/widgets/home/panel/expanded_panel.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scial/enums/event_list_state_enum.dart';
 
-import 'package:scial/models/event_model.dart';
+import 'package:scial/models/event_list_model.dart';
 import 'package:scial/providers/providers.dart';
 import 'package:scial/themes/palette.dart';
 import 'package:scial/widgets/custom_loading_indicator.dart';
@@ -17,7 +18,7 @@ class ExpandedPanel extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
 
-    final AsyncValue<List<EventModel>> events = watch(eventsWithinStreamProvider);
+    final AsyncValue<EventListModel> events = watch(eventsWithinStreamProvider);
 
     return Container(
       padding: EdgeInsets.only(top: 24.0),
@@ -66,38 +67,108 @@ class ExpandedPanel extends ConsumerWidget {
           SizedBox(height: 24.0),
           Expanded(
             child: events.when(
-              data: (List<EventModel> eventsList) => eventsList.length > 0 ? EventList(events: eventsList) : Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0),
-                  child: Text(
-                    'no_events_found'.tr(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Palette.gray400,
-                      height: 1.6
-                    )
-                  )
-                )
-              ),
-              loading: () => Center(
-                child: CustomLoadingIndicator(size: 16.0, color: Palette.gray100),
-              ), 
-              error: (Object e, StackTrace? s) => Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text(
-                  'error_loading'.tr(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Palette.gray400,
-                    height: 1.6
-                  )
-                )
-              )
+              data: (EventListModel events) {
+                switch(events.state) {
+                  case EventListStateEnum.WAITING: return LoadingEvents();
+                  case EventListStateEnum.DENIED: return LocationDenied();
+                  case EventListStateEnum.FAIL: return LocationFailed();
+                  case EventListStateEnum.SUCCESS: return events.events!.length == 0 ? NoEventsFound() : EventList(events: events.events!); 
+                }
+              },
+              loading: () => LoadingEvents(), 
+              error: (Object e, StackTrace? s) => LoadingError()
             )
           )
         ]
+      )
+    );
+  }
+}
+
+class LoadingEvents extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomLoadingIndicator(size: 16.0, color: Palette.gray100),
+    );
+  }
+}
+
+// TODO: Loaction denied for ever
+
+class LocationDenied extends StatelessWidget {
+  const LocationDenied({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Text(
+        'location_denied'.tr(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Palette.gray400,
+          height: 1.6
+        )
+      )
+    );
+  }
+}
+
+class LocationFailed extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Text(
+        'location_fail'.tr(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Palette.gray400,
+          height: 1.6
+        )
+      )
+    );
+  }
+}
+
+class LoadingError extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Text(
+        'error_loading'.tr(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Palette.gray400,
+          height: 1.6
+        )
+      )
+    );
+  }
+}
+
+class NoEventsFound extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Text(
+        'no_events_found'.tr(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Palette.gray400,
+          height: 1.6
+        )
       )
     );
   }
