713abfa6072cfb1d88e7c99a4f595ea0ab08ca3e
Florian Leeser <florian.leeser@icloud.com>
2021-08-15 15:20:57 +0200

    :necktie: Added providers
    1. Added providers for the places API.
    2. Added a provider for the search input.
    3. Added a provider for the PanelController.
    4. Added a provider for the searchIsOpen indication.

--- lib/providers/provider_classes.dart
+++ lib/providers/provider_classes.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BooleanStartingWithFalseStateNotifier extends StateNotifier<bool> {
@@ -10,4 +11,10 @@ class BooleanStartingWithTrueStateNotifier extends StateNotifier<bool> {
   BooleanStartingWithTrueStateNotifier() : super(true);
 
   void trigger() => state = !state;
+}
+
+class StringStartingWithEmptyStateNotifier extends StateNotifier<String> {
+  StringStartingWithEmptyStateNotifier() : super("");
+
+  void update(String value) => state = value;
 }
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,14 +1,23 @@
+import 'package:flutter/material.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:location/location.dart';
+import 'package:mapbox_search/mapbox_search.dart' hide Location;
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
+import 'package:scial/enums/event_list_state_enum.dart';
+import 'package:scial/enums/location_state_enum.dart';
+import 'package:scial/models/event_list_model.dart';
 import 'package:scial/models/event_model.dart';
+import 'package:scial/models/location_model.dart';
 import 'package:scial/services/auth_service.dart';
 import 'package:scial/providers/provider_classes.dart';
 import 'package:scial/services/database_service.dart';
 import 'package:scial/services/location_service.dart';
+import 'package:scial/services/places_service.dart';
 
 // FIREBASE
 
@@ -23,6 +32,10 @@ final geoflutterfireProvider = Provider<Geoflutterfire>((ref) => Geoflutterfire(
 
 final locationProvider = Provider<Location>((ref) => Location.instance);
 
+// PLACES
+
+final placesProvider = Provider<PlacesSearch>((ref) => PlacesSearch(apiKey: 'pk.eyJ1IjoiZmxlZXNlciIsImEiOiJja3M4dzV3OTAwa280Mm5wZnI3Nndjc2cxIn0.hkto7RqB_ZxhzLR7hy5law', limit: 5));
+
 // SERVICES
 
 final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref.read));
@@ -30,6 +43,8 @@ final authServiceProvider = Provider<AuthService>((ref) => AuthService(ref.read)
 final databaseServiceProvider = Provider<DatabaseService>((ref) => DatabaseService(ref.read));
 
 final locationServiceProvider = Provider<LocationService>((ref) => LocationService(ref.read));
+
+final placesServiceProvider = Provider<PlacesService>((ref) => PlacesService(ref.read));
 
 // USER
 
@@ -43,14 +58,24 @@ final signInObscureTextProvider = StateNotifierProvider.autoDispose<BooleanStart
 
 // HOME
 
-final currentLocationFutureProvider = FutureProvider<LocationData>((ref) => ref.watch(locationServiceProvider).currentLocation);
+final currentLocationFutureProvider = FutureProvider<LocationModel>((ref) => ref.watch(locationServiceProvider).currentLocation);
 
-final eventsWithinStreamProvider = StreamProvider<List<EventModel>>((ref) {
-  final AsyncValue<LocationData> locationData = ref.watch(currentLocationFutureProvider);
+final eventsWithinStreamProvider = StreamProvider<EventListModel>((ref) {
+  final AsyncValue<LocationModel> locationModel = ref.watch(currentLocationFutureProvider);
 
-  if (locationData.data != null && locationData.data!.value.latitude != null && locationData.data!.value.longitude != null) {
-    return ref.read(databaseServiceProvider).streamEventsWithin(center: GeoFirePoint(locationData.data!.value.latitude!, locationData.data!.value.longitude!), radius: double.infinity);
-  }
+  if (locationModel.data == null) return Stream.value(EventListModel(state: EventListStateEnum.WAITING));
 
-  return Stream.value(<EventModel>[]);
+  if (locationModel.data!.value.state == LocationStateEnum.DENIED) return Stream.value(EventListModel(state: EventListStateEnum.DENIED));
+
+  if (locationModel.data!.value.state == LocationStateEnum.FAIL) return Stream.value(EventListModel(state: EventListStateEnum.FAIL));
+
+  return ref.read(databaseServiceProvider).streamEventsWithin(center: GeoFirePoint(locationModel.data!.value.latitude!, locationModel.data!.value.longitude!), radius: double.infinity).map((List<EventModel> events) => EventListModel(state: EventListStateEnum.SUCCESS, events: events));
 });
+
+final placesSearchFutureProvider = FutureProvider.family<List<MapBoxPlace>, String>((ref, value) => ref.read(placesServiceProvider).searchPlace(value));
+
+final panelControllerProvider = Provider<PanelController>((ref) => PanelController());
+
+final searchInputProvider = StateNotifierProvider<StringStartingWithEmptyStateNotifier, String>((ref) => StringStartingWithEmptyStateNotifier());
+
+final searchIsOpenProvider = StateNotifierProvider<BooleanStartingWithFalseStateNotifier, bool>((ref) => BooleanStartingWithFalseStateNotifier());
