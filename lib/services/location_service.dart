2573b79d47bf4818e052dc8e7e6d8db71c4fb3c4
Florian Leeser <florian.leeser@icloud.com>
2021-08-15 15:17:32 +0200

    :passport_control: Added permission handling
    Added permission handling to the location service.

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:location/location.dart';
 
+import 'package:scial/models/location_model.dart';
 import 'package:scial/providers/providers.dart';
+import 'package:scial/enums/location_state_enum.dart';
 
 abstract class BaseLocationService {
-  Future<LocationData> get currentLocation;
+  Future<LocationModel> get currentLocation;
 }
 
 class LocationService implements BaseLocationService {
@@ -15,5 +17,26 @@ class LocationService implements BaseLocationService {
   Location get location => _read(locationProvider);
 
   @override
-  Future<LocationData> get currentLocation async => await location.getLocation();
+  Future<LocationModel> get currentLocation async {
+    bool serviceEnabled = await location.serviceEnabled();
+
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) return LocationModel(state: LocationStateEnum.DENIED);
+    }
+
+    PermissionStatus permissionStatus = await location.hasPermission();
+
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+
+      if (permissionStatus != PermissionStatus.granted) return LocationModel(state: LocationStateEnum.DENIED);
+    }
+
+    LocationData locationData = await location.getLocation();
+
+    if (locationData.latitude == null || locationData.longitude == null) return LocationModel(state: LocationStateEnum.FAIL);
+
+    return LocationModel(state: LocationStateEnum.SUCCESS, latitude: locationData.latitude!, longitude: locationData.longitude!);
+  }
 }
