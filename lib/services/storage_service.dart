a715fdf8a46db6138bc5b3cb6f4faea4319af8bf
Florian Leeser <florian.leeser@icloud.com>
2021-08-18 00:01:50 +0200

    :necktie: Created StorageService
    Created a service to interact with the Firebase Storage.

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:scial/enums/child_enum.dart';
+import 'package:scial/extensions/child_extension.dart';
+import 'package:scial/providers/providers.dart';
+
+abstract class BaseStorageService {
+  Future<String?> uploadImage(File imageFile);
+}
+
+class StorageService implements BaseStorageService {
+  final Reader _read;
+
+  const StorageService(this._read);
+
+  FirebaseStorage get storage => _read(firebaseStorageProvider);
+
+  @override
+  Future<String?> uploadImage(File imageFile) async {
+    DateTime now = DateTime.now();
+    String convertedDateTime = "${now.year.toString()}-${now.month.toString().padLeft(2,'0')}-${now.day.toString().padLeft(2,'0')}-${now.hour.toString().padLeft(2,'0')}-${now.minute.toString().padLeft(2,'0')}";
+
+    try {
+      TaskSnapshot snapshot = await storage.ref().child(ChildEnum.EVENT_IMAGES.childRef).child('$convertedDateTime.png').putFile(imageFile);
+      return await snapshot.ref.getDownloadURL();
+    } on FirebaseException {
+      print('Unknown error');
+    }
+
+    return null;
+  }
+}
