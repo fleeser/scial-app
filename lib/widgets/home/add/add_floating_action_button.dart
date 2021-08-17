0cff175eea3ecf559cada303c0934161b749753a
Florian Leeser <florian.leeser@icloud.com>
2021-08-17 21:46:31 +0200

    :necktie: Updated AddFloatingActionButton
    Updated the AddFloatingActionButton so that it shows the AddModal.

--- lib/widgets/home/add/add_floating_action_button.dart
+++ lib/widgets/home/add/add_floating_action_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:scial/modals/add_model.dart';
 import 'package:scial/themes/palette.dart';
 
 class AddFloatingActionButton extends StatelessWidget {
@@ -12,7 +13,17 @@ class AddFloatingActionButton extends StatelessWidget {
       height: kToolbarHeight - 10.0,
       width: kToolbarHeight - 10.0,
       child: RawMaterialButton(
-        onPressed: () {},
+        onPressed: () async {
+          await showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            backgroundColor: Palette.gray900,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(12.0))),
+            builder: (BuildContext context) {
+              return AddModal();
+            }
+          );
+        },
         fillColor: Palette.blue500,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
         child: Icon(
