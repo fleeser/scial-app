b9e0b563501686297440b938ba3d8a753b684cfc
Florian Leeser <florian.leeser@icloud.com>
2021-08-23 16:19:27 +0200

    :wheelchair: Created ProfileScreen route
    Created a route that navigates to the ProfileScreen when pressing on the profile image.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:scial/models/event_model.dart';
 
 import 'package:seafarer/seafarer.dart';
 
 import 'package:scial/args/details_screen_args.dart';
 import 'package:scial/screens/details/details_screen.dart';
+import 'package:scial/models/event_model.dart';
+import 'package:scial/screens/profile/profile_screen.dart';
 
 class Routes {
   static final seafarer = Seafarer();
@@ -15,7 +16,15 @@ class Routes {
         name: '/details',
         builder: (BuildContext context, BaseArguments? args, ParamMap params) {
           return DetailsScreen(args: args as DetailsScreenArgs);
-        }
+        },
+        defaultTransitions: [SeafarerTransition.slide_from_right]
+      ),
+      SeafarerRoute(
+        name: '/profile',
+        builder: (BuildContext context, BaseArguments? args, ParamMap params) {
+          return ProfileScreen();
+        },
+        defaultTransitions: [SeafarerTransition.slide_from_right]
       )
     ]);
   }
@@ -23,4 +32,6 @@ class Routes {
   static void navigateBack() => seafarer.pop();
 
   static void navigateToDetailsScreen({ required EventModel event }) => seafarer.navigate('/details', args: DetailsScreenArgs(event: event));
+
+  static void navigateToProfileScreen() => seafarer.navigate('/profile');
 }
