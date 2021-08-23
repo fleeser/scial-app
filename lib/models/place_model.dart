72704c908b023596bc2405201cec182f67740c34
Florian Leeser <florian.leeser@icloud.com>
2021-08-24 00:20:24 +0200

    :file_folder: Created PlaceModel
    Created a model that makes it easier to map data fetched from the MapBox API.

---
+++ lib/models/place_model.dart
@@ -0,0 +1,21 @@
+import 'package:mapbox_search/mapbox_search.dart';
+
+class PlaceModel {
+  final String placeName;
+  final double latitude;
+  final double longitude;
+
+  const PlaceModel({
+    required this.placeName,
+    required this.latitude,
+    required this.longitude
+  });
+
+  factory PlaceModel.fromMapBoxPlace(MapBoxPlace place) {
+    return PlaceModel(
+      placeName: place.placeName,
+      latitude: place.center[0],
+      longitude: place.center[1]
+    );
+  }
+}
