bf9f9cd7ebeadb3a0f98b9bea2a16cfb339b4ef2
Florian Leeser <florian.leeser@icloud.com>
2021-08-23 16:24:06 +0200

    :art: Added photo to ProfileButton
    Added the profile image of the currently logged in user to the ProfileButton.

--- lib/widgets/home/search/profile_button.dart
+++ lib/widgets/home/search/profile_button.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mapbox_search/mapbox_search.dart';
-import 'package:scial/providers/providers.dart';
-import 'package:scial/services/places_service.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:scial/providers/providers.dart';
+import 'package:scial/routes.dart';
 import 'package:scial/themes/palette.dart';
 
 class ProfileButton extends ConsumerWidget {
@@ -13,20 +14,65 @@ class ProfileButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
 
-    final PlacesService service = watch(placesServiceProvider);
+    final AsyncValue<String?> image = watch(userImageProvider);
 
     return SizedBox(
       height: kToolbarHeight - 10.0,
       width: kToolbarHeight - 10.0,
       child: RawMaterialButton(
-        onPressed: () async {
-          List<MapBoxPlace> list = await service.searchPlace("hallo");
-          list.forEach((element) {
-            
-          });
-        },
+        onPressed: () => Routes.navigateToProfileScreen(),
         fillColor: Palette.gray900,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0))
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+        child: image.when(
+          data: (String? image) => image != null ? ImageWidget(url: image) : null, 
+          loading: () => null, 
+          error: (Object e, StackTrace? s) => null
+        )
+      )
+    );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+
+  final String url;
+
+  const ImageWidget({ 
+    Key? key,
+    required this.url
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: url,
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) => Container(
+        height: kToolbarHeight - 10.0,
+        width: kToolbarHeight - 10.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          color: Palette.gray900,
+          image: DecorationImage(
+            image: imageProvider,
+            fit: BoxFit.cover
+          )
+        )
+      ),
+      placeholder: (BuildContext context, String url) => Container(
+        height: kToolbarHeight - 10.0,
+        width: kToolbarHeight - 10.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          color: Palette.gray900
+        )
+      ),
+      errorWidget: (BuildContext context, String url, dynamic e) => Container(
+        height: kToolbarHeight - 10.0,
+        width: kToolbarHeight - 10.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          color: Palette.gray900
+        )
       )
     );
   }
