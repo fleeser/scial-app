1a0b9bd680f0ea6ec3229979cb3af11642e9007f
Florian Leeser <florian.leeser@icloud.com>
2021-08-26 00:04:43 +0200

    :recycle: Refactored helper methods
    Refactored the helper methods.

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -2,15 +2,21 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
-bool fitsOnScreen(BuildContext context, double widgetsHeight, { bool hasAppBar = false }) => MediaQuery.of(context).size.height - MediaQuery.of(context).padding.bottom - (hasAppBar ? kToolbarHeight + MediaQuery.of(context).padding.top : 0.0) - widgetsHeight >= 0.0;
-
-double getMaxWidth(BuildContext context) {
-  double padding = MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right + 48.0;
-  
-  return MediaQuery.of(context).size.width <= 640.0 ? MediaQuery.of(context).size.width - padding : 640.0 - padding;
+double getVerticalPadding({ required BuildContext context }) {
+  final Orientation orientation = MediaQuery.of(context).orientation;
+  return 48.0 + (orientation == Orientation.portrait ? MediaQuery.of(context).padding.top + MediaQuery.of(context).padding.bottom : MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right);
 }
 
-double getTextHeight(String text, TextStyle textStyle, double maxWidth) {
+double getHorizontalPadding({ required BuildContext context }) {
+  final Orientation orientation = MediaQuery.of(context).orientation;
+  return  48.0 + (orientation == Orientation.portrait ? MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right : MediaQuery.of(context).padding.top + MediaQuery.of(context).padding.bottom);
+}
+
+double getMaxWidth({ required BuildContext context }) {
+  return MediaQuery.of(context).size.width <= 640.0 ? MediaQuery.of(context).size.width - getHorizontalPadding(context: context) : 640.0 - getHorizontalPadding(context: context);
+}
+
+double getTextHeight({ required String text, required TextStyle textStyle, required double maxWidth }) {
   TextPainter textPainter = TextPainter()
     ..text = TextSpan(text: text, style: textStyle)
     ..textDirection = ui.TextDirection.ltr
@@ -19,6 +25,17 @@ double getTextHeight(String text, TextStyle textStyle, double maxWidth) {
   return textPainter.height;
 }
 
+bool fitsOnScreen({ required BuildContext context, required double widgetsHeight, required bool hasAppBar }) {
+  return MediaQuery.of(context).size.height - getVerticalPadding(context: context) - (hasAppBar ? kToolbarHeight : 0.0) - widgetsHeight >= 0.0;
+}
+
 // TODO: Something is not right here:
 
-double getNeededScrollHeight(BuildContext context, { required double widgetsHeight, bool hasAppBar = false }) => MediaQuery.of(context).padding.top + MediaQuery.of(context).padding.bottom + (hasAppBar ? kToolbarHeight : 0.0) + widgetsHeight;
+double getNeededScrollHeight({ required BuildContext context, required double widgetsHeight, required bool hasAppBar }) {
+  final double overflowingSize = (MediaQuery.of(context).size.height - getVerticalPadding(context: context) - (hasAppBar ? kToolbarHeight : 0.0) - widgetsHeight).abs();
+  return MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top - (hasAppBar ? kToolbarHeight : 0.0) + overflowingSize;
+}
+
+
+
+//MediaQuery.of(context).padding.top + MediaQuery.of(context).padding.bottom + (hasAppBar ? kToolbarHeight : 0.0) + widgetsHeight;
